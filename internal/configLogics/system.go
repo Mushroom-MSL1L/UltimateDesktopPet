a0aaa81197c716333e81e053b0710d73eb933447
Mushroom-MSL1L <mushroom.ac.cs11@nycu.edu.tw>
2025-11-09 22:54:32 +0800

    chore : add new configuration for items and activities

--- internal/configLogics/system.go
+++ internal/configLogics/system.go
@@ -1,11 +1,21 @@
 package configLogics
 
 type System struct {
-	DBFile string `yaml:"dbfile"`
+	DBFile           string `yaml:"dbFile"`
+	PetImageDir      string `yaml:"petImageDir"`
+	ItemsDBFile      string `yaml:"itemsDBFile"`
+	ItemsDir         string `yaml:"itemsDir"`
+	ActivitiesDBFile string `yaml:"activitiesDBFile"`
+	ActivitiesDir    string `yaml:"activitiesDir"`
 }
 
 func (System) DefaultConfig() *System {
 	return &System{
-		DBFile: "./assets/db/udp.db",
+		DBFile:           "./assets/db/udp.db",
+		PetImageDir:      "./assets/petImages/default/",
+		ItemsDBFile:      "./assets/db/items.db",
+		ItemsDir:         "./assets/items/default/",
+		ActivitiesDBFile: "./assets/db/activities.db",
+		ActivitiesDir:    "./assets/activities/default/",
 	}
 }
