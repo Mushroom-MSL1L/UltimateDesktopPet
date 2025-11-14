caea5481430d9bb6c72d2c0ccb2d64c7e6edbc5f
Mushroom-MSL1L <mushroom.ac.cs11@nycu.edu.tw>
2025-11-14 13:31:02 +0800

    assets : add hamburger item assets

---
+++ assets/db/static_assets.sql
@@ -0,0 +1,14 @@
+INSERT INTO `items` (
+    `path`,
+    `name`,
+    `type`,
+    `water`,
+    `hunger`,
+    `health`,
+    `mood`,
+    `energy`,
+    `money_cost`,
+    `description`
+)
+VALUES 
+    ('hamburger', 'Hamburger', 'food', -10, 50, -5, 20, 10, -80, 'Needless to say. Yummy!');
