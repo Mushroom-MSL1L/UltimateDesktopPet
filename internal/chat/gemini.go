4cbd8de8ddb96e63d5da776540ba60b753e43e1d
Mushroom-MSL1L <mushroom.ac.cs11@nycu.edu.tw>
2025-11-27 14:29:10 +0800

    feat : add role play prompt context to make the reponse more like Real Desktop pet

--- internal/chat/gemini.go
+++ internal/chat/gemini.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/joho/godotenv"
 	"google.golang.org/genai"
@@ -14,26 +15,31 @@ func newGeminiClient() (*genai.Client, error) {
 	ctx := context.Background()
 	err := godotenv.Load(envFilePathForGemini)
 	if err != nil {
+		err = fmt.Errorf(
+			"newGeminiClient: \n\tfailed to load .env file: %w \n\n\t"+
+				"We need user to provide a valid genimi api at %s, content like \"GEMINI_API_KEY=<your_api_key>\"\n\t"+
+				"You can generate a key by the link https://aistudio.google.com/app/api-keys",
+			err, envFilePathForGemini)
 		return nil, err
 	}
 	client, err := genai.NewClient(ctx, nil)
 	if err != nil {
+		err = fmt.Errorf(
+			"newGeminiClient: \n\tfailed to load .env file: %w \n\n\t"+
+				"We need user to provide a valid genimi api at %s, content like \"GEMINI_API_KEY=<your_api_key>\"\n\t"+
+				"You can generate a key by the link https://aistudio.google.com/app/api-keys",
+			err, envFilePathForGemini)
 		return nil, err
 	}
 	return client, err
 }
 
-func (c *ChatMeta) chatWithModel(ctx context.Context) (err error) {
+func (c *ChatMeta) chatWithModel(ctx context.Context, request string) (response string, err error) {
 	result, err := c.Client.Models.GenerateContent(
 		ctx,
 		model,
-		genai.Text(c.Dialog.Request),
+		genai.Text(request),
 		nil,
 	)
-	if err != nil {
-		c.Dialog.Response = ""
-		return err
-	}
-	c.Dialog.Response = result.Text()
-	return nil
+	return result.Text(), err
 }
--- internal/chat/prompt.go
+++ internal/chat/prompt.go
@@ -1,1 +1,11 @@
 package chat
+
+func petRolePlayPrompt(rolePlayContext string, rawRequest string) string {
+	return rolePlayContext + `
+
+Now, respond to the user's message below while adhering to these guidelines:
+
+User: ` + rawRequest + `
+
+Desktop Pet:`
+}
